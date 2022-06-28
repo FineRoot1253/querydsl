3a5ed3535137f54c03b5339814af09f0c4e0f464
hongjungeun <gjhong1129@gmail.com>
2022-06-28 15:50:26 +0900

    feat(/querydsl/src/main/.../config): 기본 뷰 설정 추가

--- src/main/java/com/jungeunhong/querydsl/config/WebConfig.java
+++ src/main/java/com/jungeunhong/querydsl/config/WebConfig.java
@@ -1,6 +1,12 @@
 package com.jungeunhong.querydsl.config;
 
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/","/home");
+        registry.addViewController("/home").setViewName("/hello");
+    }
 }
