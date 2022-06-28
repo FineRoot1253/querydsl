88f8751c2f86303e697fe01097e5d9cc8bdb3e08
hongjungeun <gjhong1129@gmail.com>
2022-06-28 15:45:53 +0900

    Initial commit

---
+++ src/main/java/com/jungeunhong/querydsl/config/WebConfig.java
@@ -0,0 +1,6 @@
+package com.jungeunhong.querydsl.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+}
---
+++ src/test/java/com/jungeunhong/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,39 @@
+package com.jungeunhong.querydsl;
+
+import com.jungeunhong.querydsl.hello.Hello;
+import com.jungeunhong.querydsl.hello.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
+
+	@Test
+	void contextLoads() {
+
+		Hello hello = new Hello();
+
+		em.persist(hello);
+
+		JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(em);
+
+		QHello qHello = new QHello("h");
+		Hello fetchOne = jpaQueryFactory.selectFrom(qHello).fetchOne();
+		assertThat(fetchOne).isEqualTo(hello);
+		assertThat(fetchOne.getId()).isEqualTo(hello.getId());
+	}
+
+}
